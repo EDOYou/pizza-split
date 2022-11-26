874902d8ad7104d1d53ea7aa0c59017ec51df0e3
Taghiyev <ktaghiyev01@gmail.com>
2022-11-26 22:38:25 +0100

    pizza-split

--- src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -1,11 +1,25 @@
 package com.epam.rd.autotasks.pizzasplit;
 
+import java.util.Scanner;
+
 public class PizzaSplit {
     public static void main(String[] args) {
-        //Write a program, reading number of people and number of pieces per pizza and then
-        //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
+        Scanner sc = new Scanner(System.in);
+        int peopleNum = sc.nextInt();
+        int piece = sc.nextInt(); // slices per pizza
+        int helper = piece; // helper variable to make piece variable back
 
-
+        int pizza = 1;
+        do {
+            if (piece % peopleNum == 0) { // checking if remainder is zero
+                break;
+            } else {
+                pizza++;
+                piece = helper; // getting piece back
+                piece *= pizza;
+            }
+        } while (true);
+        System.out.println(pizza);
 
     }
 }
